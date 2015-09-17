08fa1500a18454c5c766e13b5ea4e7ade71d9446
geoteam <geoteam394@live.com>
2015-09-16 20:24:00 -0600

    ^^^

--- Source/Assets/ScriptReader.cs
+++ Source/Assets/ScriptReader.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Text;
+using System.IO;
 
-public class ScriptReader : MonoBehaviour {
+private bool Load(string fileName) {
+    // Handle any problems that might arise when reading the text
+    try {
+        string curString;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+
+        using (theReader) {
+            // While there's lines left in the text file, do this:
+            do {
+                curString = theReader.ReadLine();
+                if (curString != null) {
+                    while (theReader.Peek() != ' ')
+                    {
+
+                    }
+                }
+            } while (line != null);
+            theReader.Close();
+            return true; // Done reading, close the reader and return true to broadcast success    
+        }
+    }
+    // If anything broke in the try block, we throw an exception with information
+    // on what didn't work
+    catch (System.Exception e) {
+        System.Console.WriteLine("{0}\n", e.Message);
+        return false;
+    }
 }
